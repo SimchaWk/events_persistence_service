eeb2ade42f2724b40f1d32a0f0d1f94b33a43be0
Simcha <Simchav0@gmail.com>
2024-12-24 15:30:47 +0200

    Update formatted_date_util.py

--- app/utils/formatted_date_util.py
+++ app/utils/formatted_date_util.py
@@ -3,7 +3,3 @@ from datetime import datetime
 
 def formatted_datetime():
     return datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
-
-
-def event_timestamp_tostring(timestamp):
-    return datetime.fromtimestamp(timestamp).strftime('%Y-%m-%d %H:%M:%S')
