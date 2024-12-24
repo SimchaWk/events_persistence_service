3dde2031a406fd1ae528cc0584a085ae38b88cc9
Simcha <Simchav0@gmail.com>
2024-12-24 15:26:41 +0200

    app/config/local_files_config/local_files.py

--- app/config/local_files_config/local_files.py
+++ app/config/local_files_config/local_files.py
@@ -1,15 +1,14 @@
 from pathlib import Path
 from dotenv import load_dotenv
 
+from app.utils.formatted_date_util import formatted_datetime
 
 PROJECT_ROOT = Path(__file__).parent.parent.parent
 
 load_dotenv(PROJECT_ROOT / '.env')
 
-GLOBAL_TERRORISM_CSV = PROJECT_ROOT / 'data' / 'globalterrorismdb_0718dist.csv'
-SECONDARY_TERROR_CSV = PROJECT_ROOT / 'data' / 'RAND_Database_of_Worldwide_Terrorism_Incidents.csv'
-GLOBAL_TERRORISM_CSV_MINI = PROJECT_ROOT / 'data' / 'GLOBAL_TERRORISM_CSV.csv'
-SECONDARY_TERROR_CSV_MINI = PROJECT_ROOT / 'data' / 'SECONDARY_TERROR_CSV.csv'
+EVENTS_GRAPH_NETWORKX = PROJECT_ROOT / 'data' / f'terror_graph.pickle'
+EVENTS_GRAPH_NETWORKX_SECOND = PROJECT_ROOT / 'data' / f'terror_graph_{formatted_datetime()}.pickle'
 
 if __name__ == '__main__':
-    print(GLOBAL_TERRORISM_CSV)
+    print(EVENTS_GRAPH_NETWORKX)
