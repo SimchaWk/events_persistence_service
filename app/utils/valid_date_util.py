da6df66f86e38984ee27e88adca3095e6d85e89f
Simcha <Simchav0@gmail.com>
2024-12-24 15:30:47 +0200

    Update valid_date_util.py

--- app/utils/valid_date_util.py
+++ app/utils/valid_date_util.py
@@ -2,7 +2,6 @@ from datetime import datetime
 
 
 def is_valid_date(date_str: str) -> bool:
-    """Validate date string format"""
     try:
         datetime.strptime(date_str, '%Y-%m-%d')
         return True
